8205e649df6d389f224d1ca19493f144cf63f897
Nsquik <46610139+Nsquik@users.noreply.github.com>
2020-05-30 23:51:20 +0200

    fix deploy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,9 +1,10 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
-  app.use(
-    ["/api", "/auth"],
-    createProxyMiddleware({
-      target: "http://localhost:5000",
-    })
-  );
+  if (!process.env.NODE_ENV === "production")
+    app.use(
+      ["/api", "/auth"],
+      createProxyMiddleware({
+        target: "http://localhost:5000",
+      })
+    );
 };
