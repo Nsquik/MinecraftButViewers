aadcfcb5a82ad0803bbaf53767e0511adee98eb6
Nsquik <46610139+Nsquik@users.noreply.github.com>
2020-05-31 00:01:27 +0200

    Revert "fix deploy"
    This reverts commit 8205e649df6d389f224d1ca19493f144cf63f897.

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,10 +1,9 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
-  if (!process.env.NODE_ENV === "production")
-    app.use(
-      ["/api", "/auth"],
-      createProxyMiddleware({
-        target: "http://localhost:5000",
-      })
-    );
+  app.use(
+    ["/api", "/auth"],
+    createProxyMiddleware({
+      target: "http://localhost:5000",
+    })
+  );
 };
